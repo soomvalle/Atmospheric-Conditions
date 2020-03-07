695e8f8140db6acc7b1787e389a70c6677043d2c
soomvalle <60403747+soomvalle@users.noreply.github.com>
2020-03-07 16:12:55 +0000

    Real info temperature

---
+++ src/script.js
@@ -0,0 +1,25 @@
+function displayTemperature(response) {
+  console.log(response.data.main.temp);
+  let temperatureElement = document.querySelector("#current-temperature");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+
+  let cityElement = document.querySelector("#current-city");
+  cityElement.innerHTML = response.data.name;
+
+  let descriptionElement = document.querySelector("#current-description");
+  descriptionElement.innerHTML = response.data.weather[0].main;
+
+  let humidityElement = document.querySelector("#current-humidity");
+  humidityElement.innerHTML = response.data.main.humidity;
+
+  let windElement = document.querySelector("#current-wind");
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
+
+let apiKey = "c8637e0cfa28648d49fb4f6afe6af61b";
+let tempUnit = `metric`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Lisbon&appid=${apiKey}&units=${tempUnit}`;
+
+axios.get(apiUrl).then(displayTemperature);
+
+let now = new Date();
